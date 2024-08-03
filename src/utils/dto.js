d8c21baa4552469d0b64b5a1dd4ea7b479d28811
pauloandrecocco <pauloandrecocco@gmail.com>
2024-08-02 22:34:47 -0300

    refactor: faz pequeno ajuste de refatoramento no dto de retorno de usuario

--- src/utils/dto.js
+++ src/utils/dto.js
@@ -3,7 +3,7 @@ export const usuarioReturnDTO = (usuario) => {
     usuarioId: usuario.usuarioId,
     nome: usuario.nome,
     email: usuario.email,
-    ...(usuario?.telefone ? { telefone: usuario.telefone } : {}),
-    ...(usuario?.funcoes ? { funcoes: usuario.funcoes } : {}),
+    ...(usuario?.telefone && { telefone: usuario.telefone }),
+    ...(usuario?.funcoes && { funcoes: usuario.funcoes }),
   };
 };
